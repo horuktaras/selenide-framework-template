e413788426a032f612ceb903bfe26703af79f83d
HorukTaras <horuk.t@gmail.com>
2017-06-26 23:12:36 +0300

    Added tests

--- src/main/java/com/framework/template/BaseClass.java
+++ src/main/java/com/framework/template/BaseClass.java
@@ -2,6 +2,8 @@ package com.framework.template;
 
 import com.framework.template.data.dao.user.User;
 import com.framework.template.data.dao.user.UserDTOImpl;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Created by Noop on 21.06.2017.
@@ -23,5 +25,23 @@ public class BaseClass {
         System.out.println("User from source DB: " + userFromSourceDB.getFirstName() + " " + userFromSourceDB.getLastName());
         System.out.println("User from target DB: " + userFromTargetDB.getFirstName() + " " + userFromTargetDB.getLastName());
 
+    }
+
+    @Test
+    public void testLastNames_fail(){
+        int userID_1 = 1;
+        int userID_2 = 1;
+        User userFromSourceDB = userDTO.getUserByID(true, userID_1);
+        User userFromTargetDB = userDTO.getUserByID(false, userID_2);
+        Assert.assertEquals(userFromSourceDB.getFirstName(), userFromTargetDB.getLastName());
+    }
+
+    @Test
+    public void testLastNames_pass(){
+        int userID_1 = 2;
+        int userID_2 = 3;
+        User userFromSourceDB = userDTO.getUserByID(true, userID_1);
+        User userFromTargetDB = userDTO.getUserByID(false, userID_2);
+        Assert.assertEquals(userFromSourceDB.getFirstName(), userFromTargetDB.getFirstName());
     }
 }
